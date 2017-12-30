8bace378a8154794acdaadfea387e7a7dd04da92
cxy033077 <34953279+cxy033077@users.noreply.github.com>
2017-12-30 18:59:54 +0800

    Revert "增加膜和生成线"
    This reverts commit 21551e2e52386eba925774285fe0003fee057cfa.

--- MBR.Models/MBR.Context.cs
+++ MBR.Models/MBR.Context.cs
@@ -31,8 +31,5 @@ namespace MBR.Models
         public DbSet<SysException> SysException { get; set; }
         public DbSet<SysLog> SysLog { get; set; }
         public DbSet<User> User { get; set; }
-        public DbSet<Test> Test { get; set; }
-        public DbSet<Line> Line { get; set; }
-        public DbSet<MBRMembrane> MBRMembrane { get; set; }
     }
 }
