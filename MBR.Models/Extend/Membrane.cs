21551e2e52386eba925774285fe0003fee057cfa
cxy033077 <34953279+cxy033077@users.noreply.github.com>
2017-12-30 18:36:54 +0800

    增加膜和生成线

---
+++ MBR.Models/Extend/Membrane.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel.DataAnnotations;
+
+namespace MBR.Models
+{
+
+    [MetadataType(typeof(MembraneMetadata))]
+    public partial class MBRMembrane
+    {
+
+    }
+
+    public class MembraneMetadata
+    {
+        [Required]
+        [Display(Name = "厂商")]
+        public string Manufacturer { get; set; }
+
+        [Required]
+        [Display(Name = "更换时间")]
+        public string ChangeDate { get; set; }
+
+        [Required]
+        [Display(Name = "规格型号")]
+        public string SpecificationModel { get; set; }
+
+        [Required]
+        [Display(Name = "标题简称")]
+        public string Title { get; set; }
+
+    }
+}
