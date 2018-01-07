9cb385cb838bb702bd781612aa8809421362724f
cxy033077 <34953279+cxy033077@users.noreply.github.com>
2018-01-07 15:52:55 +0800

    清洗记录提交

---
+++ MBR.Web/Services/RecordCleanService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MBR.Models;
+
+namespace MBR.Web.Services
+{
+    public class RecordCleanService : BaseService<MBR.Models.CleanRecord>
+    {
+        public RecordCleanService() { }
+        public RecordCleanService(MBREntities db) { this.db = db; }
+
+    }
+}
